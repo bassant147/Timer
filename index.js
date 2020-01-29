bdc5d08014a914358ffe4fdaa0d2736480d72722
bassant abdelgawad <bassant.agawad@gmail.com>
2020-01-29 17:44:19 +0200

    styling the timer

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log('onStart');
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log('onTick');
+  onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset', 
+      perimeter * timeRemaining/duration - perimeter
+    );
+    
   },
   onComplete() {
     console.log('onComplete');
